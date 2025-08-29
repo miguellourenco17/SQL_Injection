5fba1cb851d408dddf4af5ae6f0a38a39e8c6e77
Miguel Lourenço <73312115+miguellourenco17@users.noreply.github.com>
2025-08-29 16:54:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-string connectionString;
-System.Data.SqlClient.SqlConnection cnn;
-connectionString = @"Data Source=xxx.database.windows.net;Initial Catalog=xxx;User ID=xxx ;Password=xxx ";
-cnn = new SqlConnection(connectionString);
-SqlCommand command = new SqlCommand ("SELECT customerid as ID,Firstname + ' ' + lastname as Name,companyname as Company, emailaddress as Email,phone FROM saleslt.customer WHERE EmailAddress = '" + txtEmail.Text + "'", cnn);
+String query = String.format(“select CPROD from table_name where CUSERID_WEBSEAL = ‘%s’”, username);
