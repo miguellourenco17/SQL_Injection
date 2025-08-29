29394b86378c25bbd8110e6e1d004fc87d01b32e
Miguel Lourenço <73312115+miguellourenco17@users.noreply.github.com>
2025-08-29 15:36:22 +0100

    Update and rename test.py to test.html

--- test.py
+++
@@ -1,3 +0,0 @@
-SQLCommand command = new SqlCommand("SELECT customerid as ID,Firstname + '' + lastname as Name,companyname as Company, emailaddressas Email,phone FROM saleslt.customer WHERE EmailAddress = @0", cnn);
-command.Parameters.Add(new SqlParameter("0", 1));
-
